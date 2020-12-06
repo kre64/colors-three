871e84b14b2fdce30322be45a6c16afc5c85d7ea
kre <krecoding@gmail.com>
2020-12-06 17:25:28 -0500

    This component could use a little more meat

--- src/Components/Timer.js
+++ src/Components/Timer.js
@@ -1,7 +1,7 @@
 import React from "react";
 
-const Timer = (props) => {
-	return <h1 className="timer">00:10:01</h1>;
+const Timer = ({ time }) => {
+	return <h1 className="timer">{time}</h1>;
 };
 
 export default Timer;
