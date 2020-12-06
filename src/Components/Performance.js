6996db5b64a3ea18a13134426dbb46d229ee90f5
kre <krecoding@gmail.com>
2020-12-06 17:25:01 -0500

    Performance levels increase every 100 secs

--- src/Components/Performance.js
+++ src/Components/Performance.js
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-const Performance = (props) => {
-	return <h1>Performance</h1>;
+const Performance = ({ seconds }) => {
+	const [performance, setPerformance] = useState("0");
+
+	const LEVELS = {
+		0: "🐣",
+	};
+
+	useEffect(() => {
+		let levelsKey = Math.floor(seconds / 100);
+		if (!LEVELS[levelsKey]) {
+			console.log(`Level ${levelsKey} not found in LEVELS!`);
+			setPerformance("placeholder perf");
+		} else {
+			setPerformance(LEVELS[levelsKey]);
+		}
+	}, [seconds, LEVELS]);
+
+	return <h1>{performance}</h1>;
 };
 
 export default Performance;
