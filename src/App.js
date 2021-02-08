539a606aa5c5231ad02fd3e5fb582eeffb31f95f
kre <krecoding@gmail.com>
2021-02-07 22:23:18 -0500

    Game is working, but needs tweaking to feel good

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import React, { useState } from "react";
-
-import "bootstrap/dist/css/bootstrap.min.css";
+import React, { useState, useEffect, useRef } from "react";
 import Header from "./components/Header";
 import GameBoard from "./components/GameBoard";
 import Modal from "react-bootstrap/Modal";
 import Button from "react-bootstrap/Button";
 
+const INITIAL_DECAY = 10;
+
 const App = () => {
 	const [game, setGame] = useState(false);
 	const [show, setShow] = useState(true);
+	const [seconds, setSeconds] = useState(0);
+	const [attempts, setAttemps] = useState(0);
+	const [threshold, setThreshold] = useState(0);
+	const [decay, setDecay] = useState(INITIAL_DECAY);
 
 	const handleClose = () => {
 		setShow(false);
 		setGame(true);
 	};
+
 	const handleShow = () => setShow(true);
+
+	useEffect(() => {
+		if (game) {
+			const interval = setInterval(() => {
+				setSeconds((seconds) => seconds + 1);
+			}, 1000);
+
+			return () => {
+				clearInterval(interval);
+			};
+		}
+	}, [game]);
+
+	const handleGameBtnClick = (clicked_right_button) => {
+		if (clicked_right_button) {
+			increaseDecay();
+		} else {
+			killGame();
+		}
+	};
+
+	const increaseDecay = () => {
+		console.log(`${seconds} + ${Math.floor(decay - 0.25)}`);
+		setThreshold(seconds + Math.floor(decay - 0.25));
+		setDecay((decay) => decay - 0.25);
+	};
+
+	const killGame = () => {
+		setGame(false);
+		setSeconds(0);
+		setAttemps((attempts) => attempts + 1);
+		handleShow();
+	};
+
+	if (seconds > threshold) {
+		increaseDecay();
+	}
 
 	return (
 		<div className="App">
-			<Header game={game} />
-			<GameBoard game={game} />
+			<Header seconds={seconds} />
+			<GameBoard
+				game={game}
+				new_threshold={threshold}
+				handleGameBtnClick={handleGameBtnClick}
+			/>
 
 			<Modal
 				show={show}
@@ -28,18 +74,15 @@ const App = () => {
 				keyboard={false}
 				centered
 			>
-				<Modal.Header closeButton>
-					<Modal.Title>How to play</Modal.Title>
-				</Modal.Header>
-				<Modal.Body>
-					Colors Three is a game that will score your reflex and
-					reaction times.
+				<Modal.Body className="justify-content-center text-center">
+					Colors Three is a game to test your reflexes.
 					<br />
 					<br />
-					Click the appropriately colored buttons as long as you can.
+					Click the button that fully lights up, missing means{" "}
+					<b>GAME OVER.</b>
 					<br />
 					<br />
-					Good luck &#128516;
+					{attempts ? `Attempt # ${attempts}` : "Good luck 😄"}
 				</Modal.Body>
 				<Modal.Footer>
 					<Button
@@ -47,7 +90,7 @@ const App = () => {
 						onClick={handleClose}
 						block
 					>
-						Play
+						{attempts ? "Go Again" : "Start Game"}
 					</Button>
 				</Modal.Footer>
 			</Modal>
